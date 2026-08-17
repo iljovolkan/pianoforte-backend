57ce9ccd3eae34a7ca7e21e82c9fbcc2921f4da7
iljovolkan <webivdesign@gmail.com>
2026-08-18 01:37:10 +0200

    Update admin.js

--- src/routes/admin.js
+++ src/routes/admin.js
@@ -7,7 +7,7 @@ const { runDailyMaintenance } = require('../cron');
 
 const router = express.Router();
 const SALT_ROUNDS = 12;
-const VALID_INSTRUMENTS = ['piano', 'gitara', 'el-gitara', 'tapani', 'peenje', 'violina'];
+const VALID_INSTRUMENTS = ['piano', 'gitara', 'el-gitara', 'tapani', 'peenje', 'violina', 'ran-razvoj'];
 
 function generateTempPassword(){
   // 12-карактерна случајна лозинка (hex), доволно силна за привремена употреба
@@ -147,4 +147,3 @@ router.get('/purchases', requireAuth, requireRole('admin'), async (req, res) => 
 });
 
 module.exports = router;
-
